04871d642cacb847e2534e5c4a9f7423e336b703
Rafael <rmaahmadov@bankofbaku.com>
2021-08-12 14:10:58 +0400

    Initial Commit

---
+++ MonumentalHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonumentalHabits
+//
+//  Created by Bob on 12.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
