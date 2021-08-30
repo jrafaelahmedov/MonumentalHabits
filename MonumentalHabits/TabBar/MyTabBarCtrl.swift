4209e3b4fe74761aa25b5fcd131db9f2f19e0fa4
Rafael Ahmedov <41021698+jrafaelahmedov@users.noreply.github.com>
2021-08-30 09:09:12 +0400

    "toolbar change"

--- MonumentalHabits/TabBar/MyTabBarCtrl.swift
+++ MonumentalHabits/TabBar/MyTabBarCtrl.swift
@@ -22,20 +22,7 @@ class MyTabBarCtrl : UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
-//        let bottomView: UIView = {
-//            let view = UIView()
-//            view.translatesAutoresizingMaskIntoConstraints = false
-//            view.backgroundColor = .white
-//            return view
-//        }()
-//
-//        view.addSubview(bottomView)
-//
-//        bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//        bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        bottomView.heightAnchor.constraint(equalToConstant: 35).isActive = true
-//
+
         tabBar.backgroundColor = UIColor.clear
         tabBar.backgroundImage = UIImage()
         tabBar.shadowImage = UIImage()
@@ -44,29 +31,24 @@ class MyTabBarCtrl : UITabBarController, UITabBarControllerDelegate {
             customizator.myTabBar = tabBar
             customizator.customize()
       
-        
-        
         let homeController = HomeViewController()
         homeController.tabBarItem = UITabBarItem(title: .none, image: UIImage(named:Utils.unselected_home_icon)?.withRenderingMode(.alwaysOriginal) , selectedImage:  UIImage(named:Utils.home_tab_icon)?.withRenderingMode(.alwaysOriginal))
         homeController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0);
-        
         
         
         let homeController2 = HomeViewController()
         homeController2.tabBarItem = UITabBarItem(title: .none, image: UIImage(named:Utils.second_tab_unselected)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named:Utils.second_tab_icon)?.withRenderingMode(.alwaysOriginal))
         homeController2.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0);
         
-        
-        
+            
         let homeController3 = HomeViewController()
         homeController3.tabBarItem = UITabBarItem(title: .none, image: UIImage(), tag: 2)
         
-//        let homeController3 = HomeViewController()
-//        homeController3.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 0)
         
         let homeController4 = HomeViewController()
         homeController4.tabBarItem = UITabBarItem(title: .none, image: UIImage(named:Utils.third_unselected)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named:Utils.third_tab_icon)?.withRenderingMode(.alwaysOriginal))
         homeController4.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0);
+        
         
         let homeController5 = HomeViewController()
         homeController5.tabBarItem = UITabBarItem(title: .none, image: UIImage(named:Utils.fourd_unselected)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named:Utils.fourt_tab_icon)?.withRenderingMode(.alwaysOriginal))
@@ -90,11 +72,8 @@ class MyTabBarCtrl : UITabBarController, UITabBarControllerDelegate {
 
         self.view.addSubview(button)
         self.view.bringSubviewToFront(button)
-        
         button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -button.frame.height-10).isActive = true
-//        button.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: 0.3).isActive = true
         button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
         button.addTarget(self, action: #selector(didTouchCenterButton(_:)), for: .touchUpInside)
     }
 
